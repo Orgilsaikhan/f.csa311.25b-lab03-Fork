4c18092ca8f5e32629945bdcfef6bafd71019480
Hugo <orgilsaikhan.g07@gmail.com>
2025-02-18 15:42:50 -0600

    Renderer class bolon Main Class-d shinechlelt oruulj zowhon Shape interface ashiglan durs zurah bolgow.

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -1,19 +1,15 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.*;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Renderer {
-    public Rectangle rectangle;
+    public Shape shape;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
-
-        // assume implementation
-
-        System.out.println("Shape printed\n" + "Its area is " + area);
+        shape.draw();
     }
 }
